443a42ac5c298eaec4283235e9f350c3bf8b75f1
Sudhanshuuu <132318176+Sudhanshuuu@users.noreply.github.com>
2024-03-24 10:39:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <BrowserRouter basename="/Maargdarshan">
+    <BrowserRouter basename="/Margdarshaan">
     <Page />
     </BrowserRouter>
     </div>
