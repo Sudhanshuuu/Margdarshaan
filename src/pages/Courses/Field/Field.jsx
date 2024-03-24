24221b91611ca4cf4599ae071ad81c8208312b23
Sudhanshuuu <132318176+Sudhanshuuu@users.noreply.github.com>
2024-03-24 15:12:13 +0530

    Update Field.jsx

--- src/pages/Courses/Field/Field.jsx
+++ src/pages/Courses/Field/Field.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import styles from "./Field.module.css";
 import HamburgerMenu from "../../../components/Navbar/NavBar";
 import FieldImg from "../Component/FieldImg";
+import loaderImg from "../../../assets/loader.gif";
 import Card2 from "../../../components/Card2/Card2";
 
 
@@ -11,6 +12,7 @@ function Field() {
     let render = useRef(true);
     let { field } = useParams();
     let [exams, setExams] = useState([]);
+    let [loader, setLoader] = useState(false);
 
 
     useEffect(() => {
@@ -19,9 +21,11 @@ function Field() {
 
                 if (render.current) {
                     render.current = false;
+                    setLoader(true);
                     let response = await fetch(`https://maargdarshan.onrender.com/courses/${field}`);
                     let data = await response.json();
-                    setExams(data);
+                    setLoader(false);
+                    setExams(data);    
                 }
             }
             catch (err) {
@@ -36,6 +40,7 @@ function Field() {
     <div className={styles["nav-bar"]}><HamburgerMenu/></div>    
         <FieldImg field={field}/>
         <div className={styles["cards"]}>
+        {loader?<img src={loaderImg} alt="loaderImg" className={styles["loader"]}/> : ""}
         {exams.map(( data, index)=>{return <Card2 val={data} key={index} field={field.slice(0,3)} className={styles["card"]}/>})}
         </div>
     </div>);
